497599018fb451162b20ed0fe78dff1cec7282e9
Merlin Beutlberger <m.beutlberger@sap.com>
2018-11-15 09:23:18 +0100

    [INTERNAL] ESLint: Allow strings to use backticks

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,8 @@ module.exports = {
         ],
         "quotes": [
             "error",
-            "double"
+            "double",
+            { "allowTemplateLiterals": true }
         ],
         "semi": [
             "error",
