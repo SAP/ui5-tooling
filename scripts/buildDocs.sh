476ff6ccd323a9f9a09cb28de5334a807011c5d4
Yavor Ivanov <yavor.ivanov@sap.com>
2022-12-02 14:38:46 +0200

    Use ENV variables with mike + docker

--- scripts/buildDocs.sh
+++ scripts/buildDocs.sh
@@ -3,17 +3,13 @@ set -e
 
 # Store docker image name
 DOCKER_IMAGE=ui5-tooling/mkdocs-material
+GIT_COMMITTER_NAME: "OpenUI5 Bot"
+GIT_COMMITTER_EMAIL: "openui5@sap.com"
 
 # If not provided, set default values for building the docs
 if [[ -z "${MIKE_VERSION}" ]]; then
 	MIKE_VERSION=v3
 	MIKE_ALIAS=latest
-fi
-
-# If not provided, set default values for building the docs
-if [[ -z "${GIT_COMMITTER_EMAIL}" ]]; then
-	GIT_COMMITTER_NAME="OpenUI5 Bot"
-	GIT_COMMITTER_EMAIL="openui5@sap.com"
 fi
 
 cd "$(dirname -- "$0")/.."
@@ -33,25 +29,8 @@ fi
 
 npm run generate-cli-doc
 
-# Setup Git config for mike & stash local settings to prevent overwrites
-LOCAL_GH_NAME=$(git config --default "" --local --get user.name)
-LOCAL_GH_EMAIL=$(git config --default "" --local --get user.email)
-RESET_GH_NAMES="${LOCAL_GH_NAME}${LOCAL_GH_EMAIL}"
-
-if [[ -z "${RESET_GH_NAMES}" ]]; then
-	echo "Setting temporary git local user.name and user.email ${GIT_COMMITTER_NAME}<${GIT_COMMITTER_EMAIL}>"
-	git config --local user.name $GIT_COMMITTER_NAME
-	git config --local user.email $GIT_COMMITTER_EMAIL
-fi
-
 # Build with MkDocs/Mike
-docker run --rm -v $(pwd):/docs $DOCKER_IMAGE mike deploy $MIKE_VERSION $MIKE_ALIAS --rebase --update-aliases
-
-if [[ -z "${RESET_GH_NAMES}" ]]; then
-	echo "Cleanup git --local records"
-	git config --local --unset user.name
-	git config --local --unset user.email
-fi
+docker run --rm -v $(pwd):/docs -e GIT_COMMITTER_NAME=$GIT_COMMITTER_NAME -e GIT_COMMITTER_EMAIL=$GIT_COMMITTER_EMAIL $DOCKER_IMAGE mike deploy $MIKE_VERSION $MIKE_ALIAS --rebase --update-aliases
 
 npm run jsdoc-generate
 
