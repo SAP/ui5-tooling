e3c618d7e148384c0c359f538416b75ea3d51b43
Yavor Ivanov <yavor.ivanov@sap.com>
2022-12-12 15:59:06 +0200

    Bugfixes

--- scripts/buildImage.sh
+++ scripts/buildImage.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
 
+cd "$(dirname -- "$0")"
+
 echo "Building image..."
-docker build -t ui5-tooling/mkdocs-material -f scripts/Dockerfile .
+docker build -t ui5-tooling/mkdocs-material -f Dockerfile .
 echo "Done building image."
 
 exit 0
