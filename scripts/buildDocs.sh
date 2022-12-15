5dd6963abfc6f553a5bbecd865f91cacd0e03471
Yavor Ivanov <yavor.ivanov@sap.com>
2022-12-15 12:18:11 +0200

    Add info to the terminal

--- scripts/buildDocs.sh
+++ scripts/buildDocs.sh
@@ -14,17 +14,17 @@ npm run generate-cli-doc
 
 
 if [[ $MIKE_VERSION ]]; then # Build with Mike (versioning)
-
+	echo "Start building & versioning Docs with Mike version: ${MIKE_VERSION}; alias: ${MIKE_ALIAS}..."
 	docker run --rm -v $(pwd):/docs --entrypoint mike \
 		--env GIT_COMMITTER_NAME="${GIT_COMMITTER_NAME}" --env GIT_COMMITTER_EMAIL="${GIT_COMMITTER_EMAIL}"  \
 		$DOCKER_IMAGE deploy $MIKE_VERSION $MIKE_ALIAS --rebase --update-aliases
 
 else # Build with MkDocs
-
+	echo "Start building Docs with MkDocs..."
 	docker run --rm -v $(pwd):/docs $DOCKER_IMAGE build
 
 fi
 
 npm run jsdoc-generate
 
-echo "Documentation build & tagged"
+echo "Documentation build"
