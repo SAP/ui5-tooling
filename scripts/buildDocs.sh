3a4dd46dca8e67a121b91eda33c5c7f7c59cebdf
Yavor Ivanov <d3xter666@users.noreply.github.com>
2022-12-16 15:37:56 +0200

    [INTERNAL] Create versioning for the documentation (#697)
    JIRA: CPOUI5FOUNDATION-565
    
    This change creates a new documentation release workflow that enables versioning.
    
    It extends the MkDocs squidfunk/mkdocs-material image by adding mike to it.
    The GitHub Actions workflow is also being extended to support the new feature.
    
    URLs of the documentation would be changed with a version number prepending the path. In order to "keep" old links available for the end users, there's a custom redirect for GH Pages.

--- scripts/buildDocs.sh
+++ scripts/buildDocs.sh
@@ -2,11 +2,29 @@
 set -e
 
 cd "$(dirname -- "$0")/.."
-
 echo "Changed directory to $(pwd)"
+
+# Store docker image name
+DOCKER_IMAGE=ui5-tooling/mkdocs-material
+
+# Build image if not existing
+./scripts/buildImage.sh
 
 npm run generate-cli-doc
 
-docker run --rm -it -v $(pwd):/docs squidfunk/mkdocs-material:6.0.2 build
+
+if [[ $MIKE_VERSION ]]; then # Build with Mike (versioning)
+	echo "Starting building & versioning Docs with Mike version: ${MIKE_VERSION}; alias: ${MIKE_ALIAS}..."
+	docker run --rm -v $(pwd):/docs --entrypoint mike \
+		--env GIT_COMMITTER_NAME="${GIT_COMMITTER_NAME}" --env GIT_COMMITTER_EMAIL="${GIT_COMMITTER_EMAIL}"  \
+		$DOCKER_IMAGE deploy $MIKE_VERSION $MIKE_ALIAS --rebase --update-aliases
+
+else # Build with MkDocs
+	echo "Starting building Docs with MkDocs..."
+	docker run --rm -v $(pwd):/docs $DOCKER_IMAGE build
+
+fi
 
 npm run jsdoc-generate
+
+echo "Documentation has been built"
--- scripts/serveDocs.sh
+++ scripts/serveDocs.sh
@@ -2,9 +2,14 @@
 set -e
 
 cd "$(dirname -- "$0")/.."
-
 echo "Changed directory to $(pwd)"
+
+# Store docker image name
+DOCKER_IMAGE=ui5-tooling/mkdocs-material
+
+# Build image if not existing
+./scripts/buildImage.sh
 
 npm run generate-cli-doc
 
-docker run --rm -it -p 8000:8000 -v $(pwd):/docs squidfunk/mkdocs-material:6.0.2
+docker run --rm -it -p 8000:8000 -v $(pwd):/docs $DOCKER_IMAGE
