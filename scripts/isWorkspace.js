a8cee056933b79147248a51d64c1a67687e807dc
Merlin Beutlberger <m.beutlberger@sap.com>
2024-02-19 13:44:00 +0100

    [INTERNAL] scripts: Use import.meta.resolve instead of require.resolve
    This change is possible now thanks to a higher minimum Node.js version

--- scripts/isWorkspace.js
+++ scripts/isWorkspace.js
@@ -1,13 +1,7 @@
 import path from "node:path";
 import {fileURLToPath} from "node:url";
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-// Using CommonsJS require.resolve as long as import.meta.resolve is experimental
-import {createRequire} from "node:module";
-const require = createRequire(import.meta.url);
-
-const cPath = require.resolve("@ui5/cli/package.json");
-const cliPath = path.relative(__dirname, path.dirname(cPath));
+const cliPath = path.relative(__dirname, fileURLToPath(import.meta.resolve("@ui5/cli/package.json")));
 
 // no workspace detected
 if (!cliPath.startsWith(path.join("..", ".."))) {
