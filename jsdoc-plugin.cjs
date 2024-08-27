92119bffad0c8c035231056b7a00dd1ce7283f24
Merlin Beutlberger <m.beutlberger@sap.com>
2024-08-27 11:22:30 +0200

    [INTERNAL] TypeScript: Remove jsdoc config files

--- jsdoc-plugin.cjs
+++
@@ -1,9 +0,0 @@
-/*
- * This plugin fixes unexpected JSDoc behavior that prevents us from using types that start with an at-sign (@).
- * JSDoc doesn't see "{@" as a valid type expression, probably as there's also {@link ...}.
- */
-exports.handlers = {
-	jsdocCommentFound: function(e) {
-		e.comment = e.comment.replace(/{@ui5\//g, "{ @ui5/");
-	}
-};
